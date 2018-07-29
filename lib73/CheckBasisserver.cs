7aa07613ad3fceeaf427aefe9844675e27febe69
chrissx <35269695+chrissxYT@users.noreply.github.com>
2018-07-29 22:33:59 +0200

    Added 3 actions as their own classes.

---
+++ lib73/CheckBasisserver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lib73
+{
+    public class CheckBasisserver : Line
+    {
+        public CheckBasisserver(string l) : base(l) { }
+    }
+}
